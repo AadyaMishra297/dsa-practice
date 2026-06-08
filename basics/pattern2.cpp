971e1830c0d9c1166adf0777040c5af473f49cd6
Aadya Mishra <aadyamishratg29@gmail.com>
2026-06-08 18:11:14 +0530

    initial commit

---
+++ basics/pattern2.cpp
@@ -0,0 +1,23 @@
+/*print pattern 
+1 1 1 
+2 2 2 
+3 3 3
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number of rows";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+       int j=1;
+        while(j<=n){
+            cout<<i<<" ";
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+    
+}
